d8b3888d9a2bef2009736a76c0381ea16ca9b0fe
James Tam <james.kit.tam@gmail.com>
2018-03-23 23:50:32 -0700

    project flow overhaul
    remade gulpfile.js
    will need to make folder structure concise

--- gulpfile.js
+++ gulpfile.js
@@ -1,47 +1,60 @@
-// We have to require our dependencies
-var gulp = require('gulp'),
-    livereload = require('gulp-livereload');
-var $ = require('gulp-load-plugins')();
+var gulp = require('gulp');
+var uglify = require('gulp-uglify');
+var autoprefixer = require('gulp-autoprefixer');
+var sourcemaps = require('gulp-sourcemaps');
+var sass = require('gulp-sass');
+var pug = require('gulp-pug');
+var coffee = require('gulp-coffee');
+var browserSync = require('browser-sync');
 
-// create a TASK to compile Pug to HTML using gulp-pug
-gulp.task('html', function() {
-   gulp.src(['./lib/pug/*.pug'])
-   .pipe($.pug({pretty: true, doctype: 'html'}))
-   .on('error', $.util.log)
-   .pipe(gulp.dest('.'));
+gulp.task('minify', function() {
+  gulp.src('js/main.js')
+  .pipe(uglify())
+  .pipe(gulp.dest('build'));
 });
 
-// create a TASK to compile CoffeeScript to JavaScript using gulp-coffee
-gulp.task('js', function() {
+gulp.task('processCSS', function() {
+  gulp.src('./css/*.css')
+  .pipe(sourcemaps.init())
+  .pipe(autoprefixer())
+  .pipe(sourcemaps.write())
+  .pipe(gulp.dest('build'));
+});
+
+gulp.task('sass', function () {
+  return gulp.src('./lib/sass/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./css'));
+});
+
+gulp.task('coffee', function() {
    gulp.src(['./lib/coffee/*.coffee'])
-   .pipe($.coffee({bare: true}))
-   .on('error', $.util.log)
+   .pipe(coffee({bare: true}))
    .pipe(gulp.dest('./js/'));
 });
 
-// create a TASK to compile Sass into CSS using gulp-sass
-gulp.task('css', function() {
-   gulp.src(['./lib/sass/*.scss'])
-   .pipe($.sass({style: 'expanded'}))
-   .on('error', $.util.log)
-   .pipe(gulp.dest('./css/'));
+gulp.task('pug', function() {
+   gulp.src(['./lib/pug/*.pug'])
+   .pipe(pug({pretty: true, doctype: 'html'}))
+   .pipe(gulp.dest('.'));
 });
 
-// create a TASK to WATCH for changes in your files
-// this will "watch" for any changes in your files and rerun gulp if necessary
+gulp.task('default', ['serve']);
+
 gulp.task('watch', function() {
-   gulp.watch(['./lib/**/*.pug'], ['html']);
-   gulp.watch(['./lib/**/*.coffee'], ['js']);
-   gulp.watch(['./lib/**/*.scss'], ['css']);
-   // Create LiveReload server
-   livereload.listen();
-
-   // Watch any files in lib/, reload on change
-   gulp.watch(['./*.html']).on('change', livereload.changed);
-   gulp.watch(['./lib/**']).on('change', livereload.changed);
+  gulp.watch('css/*.css', ['processCSS']);
+  gulp.watch('js/*.js', ['minify']);
 });
 
-// finally, create a TASK that will run all commands when typing "gulp"
-// in Terminal
-
-gulp.task('default', ['html', 'js', 'css', 'watch'], function() {});
+gulp.task('serve', ['processCSS','minify','sass','coffee','pug'], function() {
+  browserSync.init({
+    server: '.',
+    port: 3000
+  });
+  gulp.watch('./lib/sass/*.scss', ['sass']);
+  gulp.watch('css/*.css', ['processCSS']).on('change', browserSync.reload);
+  gulp.watch(['./lib/coffee/*.coffee'], ['coffee']);
+  gulp.watch('js/*.js', ['minify']).on('change', browserSync.reload);
+  gulp.watch(['./lib/pug/*.pug'], ['pug']);
+  gulp.watch('*.html').on('change', browserSync.reload);
+});
